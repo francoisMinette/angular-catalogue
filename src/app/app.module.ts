f4952be16ebddb18f718a3650501889206011e3d
Zouc <fminette@gmail.com>
2020-02-08 17:10:06 +0200

    started implementation of multi language

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +13,11 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ExpectationsComponent } from './expectations/expectations.component';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+	return new TranslateHttpLoader(http);
+}
 
 @NgModule({
 	declarations: [
@@ -27,6 +34,13 @@ import { ExpectationsComponent } from './expectations/expectations.component';
 		BrowserModule,
 		HttpClientModule,
 		AppRoutingModule,
+		TranslateModule.forRoot({
+			loader: {
+				provide: TranslateLoader,
+				useFactory: HttpLoaderFactory,
+				deps: [HttpClient],
+			},
+		}),
 		RouterModule.forRoot([
 			{ path: '', component: ProductListComponent },
 			{ path: 'about-us', component: AboutUsComponent },
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
 	selector: 'app-header',
@@ -6,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-	constructor() {}
+	constructor(private translate: TranslateService) {
+		translate.addLangs(['en', 'fr']);
+		translate.setDefaultLang('en');
+		translate.use('en');
+	}
+
+	changeLang(lang: string) {
+		console.log('in changeLang', lang);
+		this.translate.use(lang);
+	}
 
 	ngOnInit(): void {}
 }
